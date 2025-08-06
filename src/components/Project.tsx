fe34ea4ffb52e56f172df2dbdaf6e26c7b6abd30
Washifur Rahman <washifurrahman@Washifurs-MacBook-Air.local>
2025-08-06 11:38:02 +0600

    further modified

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -17,38 +17,38 @@ function Project() {
         <h1>Personal Projects</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><h2>React Frontend - Rooks Movies</h2></a>
+                <a href="https://github.com/washifur-shihan/react-project-frontend" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/washifur-shihan/react-project-frontend" target="_blank" rel="noreferrer"><h2>React Frontend - Rooks Movies</h2></a>
                 <p>Designed, developed movie finder app with semantic search with favourite showbar using React, JavaScript, RESTful APIs</p>
             </div>
             <div className="project">
-                <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><img src={mock09} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><h2>Road Object Detection and Road Segmentation</h2></a>
+                <a href="https://github.com/washifur-shihan/Thesis_T2330135" target="_blank" rel="noreferrer"><img src={mock09} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/washifur-shihan/Thesis_T2330135" target="_blank" rel="noreferrer"><h2>Road Object Detection and Road Segmentation</h2></a>
                 <p>Designed, developed a model for object detection and road segmentation simultaneously, using 4 different YOLO models and CNNs.</p>
             </div>
             <div className="project">
-                <a href="https://yujisatojr.itch.io/spacecraft" target="_blank" rel="noreferrer"><img src={mock08} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://yujisatojr.itch.io/spacecraft" target="_blank" rel="noreferrer"><h2>Rookie Academy E-Platform</h2></a>
+                <a href="https://github.com/washifur-shihan/e-learning-repo" target="_blank" rel="noreferrer"><img src={mock08} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/washifur-shihan/e-learning-repo" target="_blank" rel="noreferrer"><h2>Rookie Academy E-Platform</h2></a>
                 <p>This is an online educational platform for learning using NextJS, Node-Express, React and MongoDB. I created the entire platform from scratch using the MERN Framework.</p>
             </div>
             <div className="project">
-                <a href="https://www.datumlearn.com/" target="_blank" rel="noreferrer"><img src={mock07} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.datumlearn.com/" target="_blank" rel="noreferrer"><h2>Laptop Price Detector</h2></a>
+                <a href="https://github.com/washifur-shihan/laptop-price-predict-ML" target="_blank" rel="noreferrer"><img src={mock07} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/washifur-shihan/laptop-price-predict-ML" target="_blank" rel="noreferrer"><h2>Laptop Price Detector</h2></a>
                 <p>A price prediction model and application using machine learning algorithms such as Random Forest, Linear Regression and Voting Regressor. Proper Data Processing and features were extracted from a dataset, taken from a kaggle website and then engineered on it.</p>
             </div>
             <div className="project">
-                <a href="http://www.wemanage.jp/" target="_blank" rel="noreferrer"><img src={mock06} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="http://www.wemanage.jp/" target="_blank" rel="noreferrer"><h2>Maze Game 2D</h2></a>
+                <a href="https://github.com/washifur-shihan/maze-game-2d-pythonOpenGL" target="_blank" rel="noreferrer"><img src={mock06} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/washifur-shihan/maze-game-2d-pythonOpenGL" target="_blank" rel="noreferrer"><h2>Maze Game 2D</h2></a>
                 <p>A maze game with counter of how many moves used to complete the game. OpenGL and python framework were used in this project for its design.</p>
             </div>
             <div className="project">
-                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><img src={mock05} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><h2>Flutter Breakfast App</h2></a>
+                <a href="https://github.com/washifur-shihan/flutter-breakfast-app" target="_blank" rel="noreferrer"><img src={mock05} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/washifur-shihan/flutter-breakfast-app" target="_blank" rel="noreferrer"><h2>Flutter Breakfast App</h2></a>
                 <p>Designed a flutter application for breakfast and foods with their calory and other information with a search option.</p>
             </div>
             <div className="project">
-                <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><img src={mock04} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><h2>The Office Schema SQL</h2></a>
+                <a href="https://github.com/washifur-shihan/sql-office-schema" target="_blank" rel="noreferrer"><img src={mock04} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/washifur-shihan/sql-office-schema" target="_blank" rel="noreferrer"><h2>The Office Schema SQL</h2></a>
                 <p>Developed a Database to learn SQL as a whole. The Office series reference was taken here to implement the schema.</p>
             </div>
             
